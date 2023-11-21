49f03059422dc3d5f5983de5cc60bdf787242412
Cristhoper Mendez <51303481+Cristhoper-Mendez@users.noreply.github.com>
2023-11-20 20:29:43 -0600

    Eliminar vacuna

---
+++ src/main/resources/static/js/vacunas.js
@@ -0,0 +1,42 @@
+$(document).on("click", ".btn-delete-vacuna", function () {
+    let id = $(this).attr("data-id");
+    const url = $("#url-delete").val();
+
+    swal({
+        title: "¿Estas seguro de eliminar esta vacuna?",
+        text: "¡Una vez eliminado no se podra restaurar!",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true
+    })
+            .then((willDelete) => {
+                if (willDelete) {
+                    $.ajax({
+                        type: "POST",
+                        url: url + id,
+                        dataType: "json",
+                        success: function (respuesta) {
+                            if (!respuesta.error) {
+                                swal("La vacuna fue eliminada correctamente.", {
+                                    icon: "success"
+                                }).then(() => {
+                                    window.location.reload();
+                                });
+                            }
+                        },
+                        error: function (e) {
+                            swal("Algo salio mal en el servidor!!.", {
+                                icon: "info"
+                            });
+                            console.log("ERROR: ", e);
+                        },
+                        done: function (e) {
+                            console.log("DONE");
+                        }
+                    });
+
+
+                }
+            });
+
+});
